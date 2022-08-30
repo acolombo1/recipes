24843b2f7ca1af2ed8c6d1789eff6d46b1f2467f
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-08-30 17:20:32 -0300

    Create migrations

---
+++ db/migrate/20220830201510_create_recipes_foods.rb
@@ -0,0 +1,11 @@
+class CreateRecipesFoods < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes_foods do |t|
+      t.integer :quantity
+
+      t.timestamps
+    end
+    add_reference(:recipes_foods, :food, foreign_key: { to_table: :foods })
+    add_reference(:recipes_foods, :recipe, foreign_key: { to_table: :recipes })
+  end
+end
