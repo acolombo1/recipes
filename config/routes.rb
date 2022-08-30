bf0db3235babbce481c584cb312706b6fa20102d
Godwin Nwachukwu <nwgwin@gmail.com>
2022-08-30 22:26:17 +0100

    added routes and migrate db

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "users#index"
+
+  # get "/users", to: "users#index"
+  get "/foods", to: "foods#index"
+  get "/recipes", to: "recipes#index"
+  get "/recipes/:recipe_id", to: "recipes#show"
+  get "/public_recipes", to: "public_recipes#index"
+  get "/shopping_list", to: "shopping_list#index"
 
   # Defines the root path route ("/")
   # root "articles#index"
