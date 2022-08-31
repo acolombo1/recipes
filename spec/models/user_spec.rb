b1aafb8759f1d756301ee12a5fdb1de3e1649150
Godwin Nwachukwu <nwgwin@gmail.com>
2022-08-31 18:33:19 +0100

    add tests for recipe and user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  subject { User.new(name: 'Apple Pie') }
+
+  it 'shows that a new user created is valid' do
+    expect(subject).to be_valid
+  end
+
+  it 'shows that a new user is not valid' do
+    new_user = User.new()
+    expect(new_user).to_not be_valid
+  end
+  
+end
