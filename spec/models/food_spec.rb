f729e727ecf2f0374b464e72b05b089ae6ca687d
Godwin Nwachukwu <nwgwin@gmail.com>
2022-08-31 18:05:38 +0100

    add tests for food model

---
+++ spec/models/food_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+
+  subject { Food.new(name: 'Apple', user: User.first, price: 0.10, quantity: 500) }
+
+  it 'shows that a new food created is valid' do
+    expect(subject).to be_valid
+  end
+
+  it 'shows that a new food is valid' do
+    new_food = Food.new(name: 'Apple', user: User.first, price: -1, quantity: 500)
+    expect(new_food).to_not be_valid
+  end
+  
+end
