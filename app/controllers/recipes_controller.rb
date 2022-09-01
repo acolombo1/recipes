3202b423d9dad611ace4065a1e63bb2aa785e289
Godwin Nwachukwu <nwgwin@gmail.com>
2022-09-01 12:01:51 +0100

    add template views for recipe list and detail

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,8 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.where(user: @current_user)
+    @recipes = Recipe.where(user: User.first)
+    # @recipes = Recipe.all.where(user: @current_user)
+    # @recipes = Recipe.all
   end
 
   def show
